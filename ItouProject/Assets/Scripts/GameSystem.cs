8f2b38404a1ac51daac5cb0d96ada2439086f351
wizLucky <nagadon0424@icloud.com>
2022-10-14 12:17:51 +0900

    add lastword&startword connect

--- ItouProject/Assets/Scripts/GameSystem.cs
+++ ItouProject/Assets/Scripts/GameSystem.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class GameSystem : MonoBehaviour
 {
     private List<string> wordList = new List<string>();
+    private char lastWord = 'り';
+    [SerializeField] private TextMeshProUGUI lastWordUI;
 
     public void AddWordList(string addWord)
     {
         if (wordList.Contains(addWord))
         {
             Debug.Log("既に言われた言葉です");
-        }
-        else
+        }else if (lastWord != addWord.FirstOrDefault()) {
+            Debug.Log("「"+ lastWord + "」から始まっていません");
+        }else
         {
             wordList.Add(addWord);
             Debug.Log(wordList.Count);
+
+            lastWord = addWord.LastOrDefault();
+            lastWordUI.text = "「" + lastWord + "」から始まる";
         }
     }
 }
