df4c5f120a893ce0e192284a17299dbbbdc215ee
wizLucky <nagadon0424@icloud.com>
2022-12-03 02:40:57 +0900

    update Title Scene

--- ItouProject/Assets/Scripts/DOTween/DOMenuScreen.cs
+++ ItouProject/Assets/Scripts/DOTween/DOMenuScreen.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class DOMenuScreen : MonoBehaviour
 {
     private RectTransform rect;
-    private bool spread = false;
+    private bool spread;
 
     private void Start()
     {
@@ -21,16 +21,16 @@ public class DOMenuScreen : MonoBehaviour
             spread = false;
             Sequence sequence = DOTween.Sequence();
             sequence
-                .Append(rect.DOAnchorPos(new Vector2(0, 0), 0.5f)).SetEase(Ease.OutSine)
-                .Join(rect.DOScale(new Vector3(1, 1, 1), 0.5f));
+                .Append(rect.DOAnchorPos(new Vector2(350, 150), 0.5f)).SetEase(Ease.InSine)
+                .Join(rect.DOScale(new Vector3(0, 0, 0), 0.5f));
         }
         else
         {
             spread = true;
             Sequence sequence = DOTween.Sequence();
             sequence
-                .Append(rect.DOAnchorPos(new Vector2(350, 150), 0.5f)).SetEase(Ease.InSine)
-                .Join(rect.DOScale(new Vector3(0, 0, 0), 0.5f));
+                .Append(rect.DOAnchorPos(new Vector2(0, 0), 0.5f)).SetEase(Ease.OutSine)
+                .Join(rect.DOScale(new Vector3(1, 1, 1), 0.5f));
         }
     }
 }
