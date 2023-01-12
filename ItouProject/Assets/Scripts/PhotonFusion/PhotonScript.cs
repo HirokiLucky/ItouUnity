d4a64d5dd512947031850f5f769c1abd2791f5e0
wizLucky <nagadon0424@icloud.com>
2023-01-12 23:44:52 +0900

    add Test

--- ItouProject/Assets/Scripts/PhotonFusion/PhotonScript.cs
+++ ItouProject/Assets/Scripts/PhotonFusion/PhotonScript.cs
@@ -4,15 +4,25 @@ using System.Collections.Generic;
 using Fusion;
 using Fusion.Sockets;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PhotonScript : MonoBehaviour, INetworkRunnerCallbacks
 {
-    [SerializeField] private NetworkPrefabRef prefabRef;
+    [SerializeField] private NetworkPrefabRef prefabHost;
+    [SerializeField] private NetworkPrefabRef prefabClient;
     
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
         Debug.Log("aaaaaaaaa");
-        runner.Spawn(prefabRef, Vector3.zero, Quaternion.identity, player);
+        if (runner.IsClient)
+        {
+            runner.Spawn(prefabClient, new Vector3(6, -3.2f,0), Quaternion.identity, player);
+        }
+        else
+        {
+            runner.Spawn(prefabClient, new Vector3(-6, -3.2f, 0), Quaternion.identity, player);
+        }
+        
     }
 
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
@@ -87,6 +97,8 @@ public class PhotonScript : MonoBehaviour, INetworkRunnerCallbacks
         _networkRunner.StartGame(new StartGameArgs()
         {
             GameMode = GameMode.AutoHostOrClient,
+            SessionName = "PlayRoom",
+            Scene = SceneManager.GetActiveScene().buildIndex
             //SceneObjectProvider = GetComponent<NetworkSceneManagerDefault>()
         });
     }
