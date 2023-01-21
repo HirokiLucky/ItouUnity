aaeae4b27f1c7a2fae4e74ccaa5e62a0e71664c9
wizLucky <nagadon0424@icloud.com>
2023-01-21 11:38:31 +0900

    finish Select First Attack

--- ItouProject/Assets/Scripts/DOTween/DOGameStart.cs
+++ ItouProject/Assets/Scripts/DOTween/DOGameStart.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+using Sequence = DG.Tweening.Sequence;
 
 public class DOGameStart : MonoBehaviour
 {
@@ -19,6 +23,7 @@ public class DOGameStart : MonoBehaviour
     [SerializeField] private RectTransform yazirushiParent;
     [SerializeField] private GameObject black;
     private Image blackImage;
+    public bool firstAttackVariable;
 
     private Vector3 origin = new Vector3(0, -5, -1);
     private Vector3 start = new Vector3(0, 3.2f, -1);
@@ -40,17 +45,35 @@ public class DOGameStart : MonoBehaviour
         Sequence sequence = DOTween.Sequence();
         sequence
             .AppendInterval(1f)
-            .Append(cameraObject.transform.DOMove(origin, 3)).SetEase(Ease.InSine);
+            .Append(cameraObject.transform.DOMove(origin, 3)).SetEase(Ease.InSine)
+            .OnComplete(() => Judge());
     }
 
     public void Judge()
     {
+        int angle = 0;
+        firstAttackVariable = DecideFirstAttack();
         parent.SetActive(true);
         black.SetActive(true);
         Sequence sequence = DOTween.Sequence();
         sequence
             .Append(blackImage.DOColor(new Color(0, 0, 0, 0.33f), 0.5f))
             .Join(parentRect.DOAnchorPosY(0, 0.5f).SetEase(Ease.OutElastic))
-            .Append(yazirushiParent.DOLocalRotate(new Vector3(0,0,3600), 5,  RotateMode.FastBeyond360).SetEase(Ease.OutExpo));
+            .AppendCallback(() =>
+            {
+                if (firstAttackVariable) yazirushiParent.DOLocalRotate(new Vector3(0, 0, 3690), 5, RotateMode.FastBeyond360).SetEase(Ease.OutExpo);
+                else yazirushiParent.DOLocalRotate(new Vector3(0, 0, 3510), 5, RotateMode.FastBeyond360).SetEase(Ease.OutExpo);
+            })
+            .AppendInterval(6)
+            .Append(parentRect.DOAnchorPosY(300, 0.5f).SetEase(Ease.InOutQuint))
+            .OnComplete(() =>
+            {
+                parent.SetActive(false);
+            });
+    }
+
+    bool DecideFirstAttack()
+    {
+        return Random.Range(0, 2) == 0;
     }
 }
