8ec96afb4b7e7ddfcd5d07fc1828401a41f1d907
wizLucky <nagadon0424@icloud.com>
2023-02-10 00:50:04 +0900

    add SEScript

---
+++ ItouProject/Assets/Scripts/Sound/SoundScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    public AudioClip sound1;
+    AudioSource audioSource;
+
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update () {
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+            //音(sound1)を鳴らす
+            audioSource.PlayOneShot(sound1);
+        }
+    }
+}
