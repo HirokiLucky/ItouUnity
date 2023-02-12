5608e59041b89dab049b86bf2aa3dfdb3930d7ef
wizLucky <nagadon0424@icloud.com>
2023-02-12 23:07:21 +0900

    add UI mp3

--- ItouProject/Assets/Scripts/Sound/SoundScript.cs
+++ ItouProject/Assets/Scripts/Sound/SoundScript.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class SoundScript : MonoBehaviour
 {
-    public AudioClip sound1;
+    public AudioClip bat;
+    public AudioClip dropJudge;
+    public AudioClip roll;
+    public AudioClip finishRoll;
+    public AudioClip slowMotion;
+    public AudioClip sound6;
+    public AudioClip sound7;
+    public AudioClip sound8;
+    public AudioClip sound9;
+    public AudioClip sound10;
     AudioSource audioSource;
 
     void Start () {
         audioSource = GetComponent<AudioSource>();
     }
-
-    void Update () {
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            //音(sound1)を鳴らす
-            audioSource.PlayOneShot(sound1);
-        }
+    
+    // シーン開始のコウモリエフェクト用
+    public void BatFly()
+    {
+        audioSource.PlayOneShot(bat);
     }
+    
+    // 先行後攻のUI用
+    public void DropJudge()
+    {
+        audioSource.PlayOneShot(dropJudge);
+    }
+    
+    public void Roll()
+    {
+        audioSource.PlayOneShot(roll);
+    }
+    
+    public void FinishRoll()
+    {
+        audioSource.PlayOneShot(finishRoll);
+    }
+    
+    // ターン開始UI用
+    public void StartTurn()
+    {
+        audioSource.PlayOneShot(slowMotion);
+    }
+    
+    // 認識文字用
+    
 }
